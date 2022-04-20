2b7db2ef1ae8dd6bf1f86ccfa2e2ddc4ab883d3c
Mark de Neut Brossois <mdeneutbrossois@bol.com>
2022-04-20 11:33:58 +0200

    dev/debug-ui: Added debug-ui lesson with lil-gui and gsap!

--- vue/src/utils/route.constants.ts
+++ vue/src/utils/route.constants.ts
@@ -5,6 +5,7 @@ import BasicAnimations from "@/views/BasicAnimations.vue";
 import BasicCameras from "@/views/BasicCameras.vue";
 import BasicFullscreen from "@/views/BasicFullscreen.vue";
 import BasicGeometries from "@/views/BasicGeometries.vue";
+import BasicDebugUi from "@/views/BasicDebugUi.vue";
 
 export const RouteConstants: { title: string, sectionTitle?: boolean, url: string, component: Component }[] = [
   {title: 'Basics', sectionTitle: true, url: '', component: {} as Component},
@@ -13,5 +14,6 @@ export const RouteConstants: { title: string, sectionTitle?: boolean, url: strin
   {title: "Animations", url: "/animations", component: BasicAnimations},
   {title: "Camera's", url: "/cameras", component: BasicCameras},
   {title: "Fullscreen and resizing", url: "/fullscreen", component: BasicFullscreen},
-  {title: "Geometries", url: "/geometries", component: BasicGeometries}
+  {title: "Geometries", url: "/geometries", component: BasicGeometries},
+  {title: "Debug UI", url: "/debug-ui", component: BasicDebugUi},
 ]
