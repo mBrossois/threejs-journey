29b886a9162bd0b75b65e2e845ad8861d25b635e
Mark de Neut Brossois <mdeneutbrossois@bol.com>
2022-04-22 15:19:08 +0200

    dev/debug-ui: Added loading manager, with textureLoader and texture transformation

--- vue/src/views/BasicTextures.vue
+++ vue/src/views/BasicTextures.vue
@@ -13,15 +13,53 @@ import * as dat from 'lil-gui'
 import gsap from 'gsap'
 
 // Texture
-const textureLoader = new THREE.TextureLoader()
-const texture = textureLoader.load('/textures/door/color.jpg')
+const loadingManager = new THREE.LoadingManager()
+loadingManager.onStart = () => {
+  console.log('loading started')
+}
+loadingManager.onLoad = () => {
+  console.log('loading finished')
+}
+loadingManager.onProgress = () => {
+  console.log('loading in progress')
+}
+loadingManager.onError = (e: any) => {
+  console.log('loading error', e)
+}
+
+// With tinyPNG can make img smaller, but might look worse
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const colorTexture = textureLoader.load('/textures/minecraft.png')
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('/textures/door/height.jpg')
+const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 3
+//
+// colorTexture.wrapS = THREE.RepeatWrapping
+// colorTexture.wrapT = THREE.MirroredRepeatWrapping
+//
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
+
+// colorTexture.rotation = Math.PI * .25
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
+
+colorTexture.generateMipmaps = false
+colorTexture.minFilter = THREE.NearestFilter
+colorTexture.magFilter = THREE.NearestFilter
 
 // Scene
 const scene: Scene = new THREE.Scene()
 
 // Object
-const geometry: BoxGeometry = new THREE.BoxGeometry(1, 1, 1, 3, 3, 3)
-const material = new THREE.MeshBasicMaterial({map: texture})
+const geometry: BoxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({map: colorTexture})
 
 const mesh = new THREE.Mesh(geometry, material)
 
