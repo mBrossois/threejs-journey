81ca374e321f64fd7f96b56351e408c869669351
Mark de Neut Brossois <mdeneutbrossois@bol.com>
2022-04-26 11:25:02 +0200

    dev/materials: Lesson done!

--- vue/src/views/BasicMaterials.vue
+++ vue/src/views/BasicMaterials.vue
@@ -5,14 +5,14 @@
 <script lang="ts">
 import {Options, Vue} from "vue-class-component";
 import * as THREE from "three";
-import {BoxGeometry, PerspectiveCamera, Scene, TextureLoader, WebGLRenderer} from "three";
+import {PerspectiveCamera, Scene, WebGLRenderer} from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import {FullScreenDocument, FullScreenDocumentElement} from "@/types/fullscreen.type";
 import {fullscreenUtil} from "@/utils/fullscreen.util";
 import * as dat from 'lil-gui'
-import gsap from 'gsap'
 
 const textureLoader = new THREE.TextureLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
 
 const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
 const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
@@ -21,8 +21,21 @@ const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
 const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
 const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-const gradients3Texture = textureLoader.load('/textures/gradients/3.jpg')
 const matcapsTexture = textureLoader.load('/textures/matcaps/8.png')
+const gradientsTexture = textureLoader.load('/textures/gradients/3.jpg')
+gradientsTexture.minFilter = THREE.NearestFilter
+gradientsTexture.magFilter = THREE.NearestFilter
+// Can deactivate with NearestFilter
+gradientsTexture.generateMipmaps = false
+
+const environmentMapTexture = cubeTextureLoader.load([
+  'textures/environmentMaps/3/py.jpg',
+  'textures/environmentMaps/3/nx.jpg',
+  'textures/environmentMaps/3/px.jpg',
+  'textures/environmentMaps/3/ny.jpg',
+  'textures/environmentMaps/3/pz.jpg',
+  'textures/environmentMaps/3/nz.jpg',
+])
 
 // Scene
 const scene: Scene = new THREE.Scene()
@@ -48,24 +61,54 @@ const scene: Scene = new THREE.Scene()
 
 // const material = new THREE.MeshLambertMaterial()
 
-const material = new THREE.MeshPhongMaterial()
-material.shininess = 100
-material.specular = new THREE.Color(0x1188ff)
+// const material = new THREE.MeshPhongMaterial()
+// material.shininess = 100
+// material.specular = new THREE.Color(0x1188ff)
+
+// const material = new THREE.MeshToonMaterial()
+// material.gradientMap = gradientsTexture
+
+// const material = new THREE.MeshStandardMaterial()
+// material.metalness = 0
+// material.roughness = 1
+// material.map = doorColorTexture
+// material.aoMap = doorAmbientOcclusionTexture
+// material.aoMapIntensity = 1
+// material.displacementMap = doorHeightTexture
+// material.displacementScale = 0.05
+// material.metalnessMap = doorMetalnessTexture
+// material.roughnessMap = doorRoughnessTexture
+// material.normalMap = doorNormalTexture
+// material.normalScale.set(0.5, 0.5 )
+// material.alphaMap = doorAlphaTexture
+// material.transparent = true
+
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.7
+material.roughness = 0.2
+material.envMap = environmentMapTexture
 
 const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.SphereGeometry(0.5, 64, 64),
     material
 )
+
+sphere.geometry.setAttribute('uv2', new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2))
 
 const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(1, 1),
+    new THREE.PlaneGeometry(1, 1, 100, 100),
     material
 )
 
+plane.geometry.setAttribute('uv2', new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2))
+
 const torus = new THREE.Mesh(
-    new THREE.TorusGeometry(.3, .2, 16, 32),
+    new THREE.TorusGeometry(.3, .2, 64, 128),
     material
 )
+
+torus.geometry.setAttribute('uv2', new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2))
+
 
 @Options({})
 export default class BasicMaterials extends Vue {
@@ -83,6 +126,8 @@ export default class BasicMaterials extends Vue {
 
   ambientLight = new THREE.AmbientLight(0xffffff, .5)
   pointLight = new THREE.PointLight(0xffffff, .5)
+
+  gui = new dat.GUI({width: 300})
 
   doc = document as FullScreenDocument
 
@@ -106,6 +151,14 @@ export default class BasicMaterials extends Vue {
     this.renderer.render(scene, this.camera)
 
     window.requestAnimationFrame(this.tick)
+  }
+
+  debug() {
+    this.gui.add(material, 'wireframe')
+    this.gui.add(material, 'metalness').min(0).max(1).step(0.0001)
+    this.gui.add(material, 'roughness').min(0).max(1).step(0.0001)
+    this.gui.add(material, 'aoMapIntensity').min(0).max(2).step(0.0001)
+    this.gui.add(material, 'displacementScale').min(0).max(1).step(0.0001)
   }
 
   mounted() {
@@ -146,6 +199,9 @@ export default class BasicMaterials extends Vue {
 
       this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
     })
+
+    // Debug
+    this.debug()
 
     // Tick
     this.tick()
