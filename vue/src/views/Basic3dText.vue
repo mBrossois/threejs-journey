c649782b0890f2b3c902cad9b6847557a14fb268
Mark de Neut Brossois <mdeneutbrossois@bol.com>
2022-04-29 11:59:34 +0200

    dev/3d-text: Added 3d text with different randomnly placed donuts

--- vue/src/views/Basic3dText.vue
+++ vue/src/views/Basic3dText.vue
@@ -5,25 +5,78 @@
 <script lang="ts">
 import {Options, Vue} from "vue-class-component";
 import * as THREE from "three";
-import {PerspectiveCamera, Scene, WebGLRenderer} from "three";
+import {Mesh, PerspectiveCamera, Scene, WebGLRenderer} from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import {FullScreenDocument, FullScreenDocumentElement} from "@/types/fullscreen.type";
 import {fullscreenUtil} from "@/utils/fullscreen.util";
 import * as dat from 'lil-gui'
+import {FontLoader} from "three/examples/jsm/loaders/FontLoader";
+import {TextGeometry} from "three/examples/jsm/geometries/TextGeometry";
 
+// Textures
 const textureLoader = new THREE.TextureLoader()
-const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+
+// Fonts
+const fontLoader = new FontLoader()
+
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json ',
+    (font) => {
+      const textGeometry =  new TextGeometry(
+          'Mark Brossois',
+          {
+            font: font,
+            size: 0.5,
+            height: 0.2,
+            curveSegments: 5,
+            bevelEnabled: true,
+            bevelThickness: 0.03,
+            bevelSize: 0.02,
+            bevelOffset: 0,
+            bevelSegments: 4
+          }
+      )
+      const material = new THREE.MeshMatcapMaterial({matcap: matcapTexture})
+      // textMaterial.wireframe = true
+
+      // Center text
+      // textGeometry.computeBoundingBox()
+      // textGeometry.translate(
+      //     - (textGeometry.boundingBox?.max.x - 0.02) * 0.5, // Subtract bevelSize
+      //     - (textGeometry.boundingBox?.max.y - 0.02) * .5, // Subtract bevelSize
+      //     - (textGeometry.boundingBox?.max.z -0.03) * .5 // Subtract bevelThickness
+      // )
+      textGeometry.center()
+
+      const text = new THREE.Mesh(textGeometry, material)
+      scene.add(text)
+
+      const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+
+      // Add random objects
+      for(let i = 0; i < 100; i++) {
+        const donut = new Mesh(donutGeometry, material)
+        donut.position.x = (Math.random() -.5) * 10
+        donut.position.y = (Math.random() -.5) * 10
+        donut.position.z = (Math.random() -.5) * 10
+
+        donut.rotation.x = Math.random() * Math.PI
+        donut.rotation.y = Math.random() * Math.PI
+
+        const scale = Math.random()
+        donut.scale.set(scale, scale, scale)
+        scene.add(donut)
+      }
+    }
+)
 
 // Scene
 const scene: Scene = new THREE.Scene()
 
 // Objects
 const material = new THREE.MeshBasicMaterial()
-
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    material
-)
 
 @Options({})
 export default class Basic3dText extends Vue {
@@ -56,17 +109,19 @@ export default class Basic3dText extends Vue {
   }
 
   debug() {
+    this.gui.close()
     this.gui.add(material, 'wireframe')
   }
 
   mounted() {
-    // objects
-    scene.add(cube)
-
     //camera
     this.camera.position.set(1, 1, 1)
     // this.camera.position.z = 2
     scene.add(this.camera)
+
+    // Axes helper
+    const axesHelper = new THREE.AxesHelper()
+    scene.add(axesHelper)
 
     //renderer
     this.renderer = new THREE.WebGLRenderer({canvas: this.$refs.webgl});
