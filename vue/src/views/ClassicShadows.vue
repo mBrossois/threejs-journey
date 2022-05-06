e0f62026b620c626bd35677f8d4a525d618ca9b7
Mark de Neut Brossois <mdeneutbrossois@bol.com>
2022-05-06 11:57:22 +0200

    dev/shadows: Bakedshadows

--- vue/src/views/ClassicShadows.vue
+++ vue/src/views/ClassicShadows.vue
@@ -6,7 +6,16 @@
 <script lang="ts">
 import {Options, Vue} from "vue-class-component";
 import * as THREE from "three";
-import {Mesh, MeshStandardMaterial, PerspectiveCamera, Scene, WebGLRenderer} from "three";
+import {
+  Mesh, MeshBasicMaterial,
+  MeshStandardMaterial,
+  PCFSoftShadowMap,
+  PerspectiveCamera,
+  PlaneGeometry,
+  Scene,
+  Texture,
+  WebGLRenderer
+} from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import {FullScreenDocument, FullScreenDocumentElement} from "@/types/fullscreen.type";
 import {fullscreenUtil} from "@/utils/fullscreen.util";
@@ -31,13 +40,21 @@ export default class ClassicShadows extends Vue {
 
   material= new THREE.MeshStandardMaterial()
 
+  // Textures
+  bakedShadow: Texture
+  simpleShadow: Texture
+
   sphere: Mesh
   plane: Mesh
+  sphereShadow: Mesh<PlaneGeometry, MeshBasicMaterial>
 
   // Low cost
-  ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
   // Medium cost
-  directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+  directionalLight = new THREE.DirectionalLight(0xffffff, 0.3)
+  pointLight = new THREE.PointLight(0xffffff, 0.3)
+  // High cost
+  spotLight = new THREE.SpotLight(0xffffff, 0.3, 10, Math.PI * 0.3)
 
   gui = new dat.GUI({width: 300})
 
@@ -48,7 +65,18 @@ export default class ClassicShadows extends Vue {
   tick() {
     const elapsedTime = this.clock.getElapsedTime()
 
-    // Update controls
+    // Move object
+    this.sphere.position.x = Math.cos(elapsedTime) * 1.5
+    this.sphere.position.y = Math.abs(Math.sin(elapsedTime * 3))
+    this.sphere.position.z = Math.sin(elapsedTime) * 1.5
+
+    // Update shadow
+    this.sphereShadow.position.x = this.sphere.position.x
+    this.sphereShadow.position.z = this.sphere.position.z
+    this.sphereShadow.material.opacity = (1 - this.sphere.position.y) * 0.3
+
+
+        // Update controls
     this.controls.update()
 
     //renderer
@@ -69,10 +97,18 @@ export default class ClassicShadows extends Vue {
 
   }
 
+  setupTextures() {
+    const textureLoader = new THREE.TextureLoader()
+    this.bakedShadow = textureLoader.load('/textures/shadows/bakedShadow.jpg')
+    this.simpleShadow = textureLoader.load('/textures/shadows/simpleShadow.jpg')
+  }
+
   setupLights() {
     this.directionalLight.position.set(2, 2, -1)
+    this.spotLight.position.set(0, 2, 2)
+    this.pointLight.position.set(-1, 1, 0)
 
-    this.scene.add(this.ambientLight, this.directionalLight)
+    this.scene.add(this.ambientLight, this.directionalLight, this.spotLight, this.spotLight.target, this.pointLight)
   }
 
   setupMaterial() {
@@ -88,22 +124,41 @@ export default class ClassicShadows extends Vue {
 
     this.plane = new THREE.Mesh(
         new THREE.PlaneGeometry(5, 5),
+        // Map for static scenes/objects
+        // new THREE.MeshBasicMaterial({map: this.bakedShadow})
         this.material
     )
     this.plane.rotation.x = -Math.PI * 0.5
     this.plane.position.y = -0.5
 
-    this.scene.add(this.sphere, this.plane)
+    // Shadow of sphere on the plane
+    this.sphereShadow = new THREE.Mesh(
+        new THREE.PlaneGeometry(1.5, 1.5),
+        new THREE.MeshBasicMaterial({
+          color: 0x000000,
+          transparent: true,
+          alphaMap: this.simpleShadow
+        })
+    )
+    this.sphereShadow.rotation.x = - Math.PI * 0.5
+    this.sphereShadow.position.y = this.plane.position.y + 0.01
+
+    this.scene.add(this.sphere, this.sphereShadow, this.plane)
 
   }
 
   setupShadow() {
-    this.renderer.shadowMap.enabled = true
+    // To cast shadows
+    this.renderer.shadowMap.enabled = false
     this.sphere.castShadow = true
     this.directionalLight.castShadow = true
+    this.spotLight.castShadow = true
+    this.pointLight.castShadow = true
 
+    // To receive shadows
     this.plane.receiveShadow = true
 
+    // Shadow camera settings
     this.directionalLight.shadow.mapSize.set(1024, 1024)
     // Camera is orthogrophic camera
     this.directionalLight.shadow.camera.near = 1
@@ -112,11 +167,27 @@ export default class ClassicShadows extends Vue {
     this.directionalLight.shadow.camera.right = 2
     this.directionalLight.shadow.camera.top = 2
     this.directionalLight.shadow.camera.bottom = -2
-    this.directionalLight.shadow.radius = 10
+    // this.directionalLight.shadow.radius = 10
+    this.spotLight.shadow.mapSize.set(1024, 1024)
+    this.spotLight.shadow.camera.fov = 30
+    this.spotLight.shadow.camera.near = 1
+    this.spotLight.shadow.camera.far = 6
+    // Point light
+    this.pointLight.shadow.mapSize.set(1024, 1024)
+    this.pointLight.shadow.camera.near = 0.1
+    this.pointLight.shadow.camera.far = 5
 
+    // Camera helpers
     const directionalLightCameraHelper = new THREE.CameraHelper(this.directionalLight.shadow.camera)
     directionalLightCameraHelper.visible = false
-    this.scene.add(directionalLightCameraHelper)
+    const spotlightCameraHelper = new THREE.CameraHelper(this.spotLight.shadow.camera)
+    spotlightCameraHelper.visible = false
+    const pointLightCameraHelper = new THREE.CameraHelper(this.pointLight.shadow.camera)
+    pointLightCameraHelper.visible = false
+    this.scene.add(directionalLightCameraHelper, spotlightCameraHelper, pointLightCameraHelper)
+
+    // Shadow type
+    this.renderer.shadowMap.type = PCFSoftShadowMap
   }
 
   setupRenderer() {
@@ -142,6 +213,9 @@ export default class ClassicShadows extends Vue {
   mounted() {
     // Initialize scene
     this.scene = new THREE.Scene()
+
+    // Load texture
+    this.setupTextures()
 
     // Lights
     this.setupLights()
