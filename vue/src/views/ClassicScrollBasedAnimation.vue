8eaf6158644b59577bc8637404cc095996454a9c
mBrossois <git@github.com:mBrossois>
2022-08-26 09:51:42 +0200

    Added scroll

--- vue/src/views/ClassicScrollBasedAnimation.vue
+++ vue/src/views/ClassicScrollBasedAnimation.vue
@@ -15,7 +15,7 @@
 <script lang="ts">
 import {Options, Vue} from "vue-class-component";
 import * as THREE from "three";
-import {BoxGeometry, Material, Mesh, MeshBasicMaterial, MeshLambertMaterial, MeshStandardMaterial, PerspectiveCamera, Points, Scene, Texture, Vector2, WebGLRenderer} from "three";
+import {BoxGeometry, ConeGeometry, DirectionalLight, Material, Mesh, MeshBasicMaterial, MeshLambertMaterial, MeshStandardMaterial, MeshToonMaterial, NearestFilter, PerspectiveCamera, Points, Scene, Texture, TorusGeometry, TorusKnotGeometry, Vector2, WebGLRenderer} from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import {FullScreenDocument, FullScreenDocumentElement} from "@/types/fullscreen.type";
 import {fullscreenUtil} from "@/utils/fullscreen.util";
@@ -37,8 +37,20 @@ export default class ClassicScrollBasedAnimation extends Vue {
   // fov between 45 & 75 || only shows objects between near and far values
   camera: PerspectiveCamera = new THREE.PerspectiveCamera(75, this.sizes.width / this.sizes.height, 0.1, 100);
 
+  // Lights
+  directionalLight: DirectionalLight
+
   // objects
-  cube: Mesh
+  mesh1: Mesh
+  mesh2: Mesh
+  mesh3: Mesh
+
+  sectionMeshes: Mesh[]
+
+  objectsDistance = 4
+
+  // Material
+  material: MeshToonMaterial
 
   scene: Scene
 
@@ -54,7 +66,7 @@ export default class ClassicScrollBasedAnimation extends Vue {
   mouse:Vector2
 
   // Textures
-  startTexture: Texture
+  gradientTexture: Texture
 
   gui = new dat.GUI({width: 300})
 
@@ -62,8 +74,19 @@ export default class ClassicScrollBasedAnimation extends Vue {
 
   clock = new THREE.Clock();
 
+  // Scroll
+  scrollY = window.scrollY
+
   tick() {
     const elapsedTime = this.clock.getElapsedTime()
+
+    // Move camera
+    this.camera.position.y = - this.scrollY / this.sizes.height * this.objectsDistance
+
+    for(const meshObject of this.sectionMeshes) {
+      meshObject.rotation.x = elapsedTime * 0.1
+      meshObject.rotation.y = elapsedTime * 0.12
+    }
 
     //renderer
     this.renderer.render(this.scene, this.camera)
@@ -74,29 +97,59 @@ export default class ClassicScrollBasedAnimation extends Vue {
   debug() {
     this.gui.close()
 
-    this.gui.addColor(this.parameters, 'materialColor')
+    this.gui.addColor(this.parameters, 'materialColor').onChange(() => {
+      this.material.color.set(this.parameters.materialColor)
+    })
 
   }
 
   setupLights() {
-
     console.log('lights')
+
+    this.directionalLight = new DirectionalLight('#ffffff', 0.8)
+    this.directionalLight.position.set(1, 1, 0)
+
+    this.scene.add(this.directionalLight)
 
   }
 
   setupMaterial() {
     console.log('materials')
+    this.material = new MeshToonMaterial({
+      color: this.parameters.materialColor,
+      gradientMap: this.gradientTexture
+    })
   }
 
   setupObjects() {
    console.log('objects')
 
-   this.cube = new Mesh(
-    new BoxGeometry(1, 1, 1),
-    new MeshBasicMaterial({ color: '#ff0000'})
+   const mesh1 = new Mesh(
+    new TorusGeometry(1, 0.4, 16, 60),
+    this.material
+   )
+   
+   const mesh2 = new Mesh(
+    new ConeGeometry(1, 2, 32),
+    this.material
+   )
+   
+   const mesh3 = new Mesh(
+    new TorusKnotGeometry(0.8, 0.35, 100, 16),
+    this.material
    )
 
-   this.scene.add(this.cube)
+   mesh1.position.y = - this.objectsDistance * 0
+   mesh2.position.y = - this.objectsDistance * 1
+   mesh3.position.y = - this.objectsDistance * 2
+
+  mesh1.position.x = 1
+  mesh2.position.x = - 1
+  mesh3.position.x = 1
+
+   this.sectionMeshes = [mesh1, mesh2, mesh3]
+
+   this.scene.add(mesh1, mesh2, mesh3)
 
   }
 
@@ -109,26 +162,11 @@ export default class ClassicScrollBasedAnimation extends Vue {
   setupTextures() {
     const textureLoader = new THREE.TextureLoader()
 
-    // Door
-    this.startTexture = textureLoader.load('/textures/particles/2.png')
+    // gradient
+    this.gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+    this.gradientTexture.magFilter = NearestFilter
 
   }
-
-  setupMouse() {
-    this.mouse = new Vector2() 
-
-    window.addEventListener('mousemove', (_event) => {
-      this.mouse.x = _event.clientX / this.sizes.width * 2 - 1
-      this.mouse.y = - (_event.clientY / this.sizes.height) * 2 + 1
-    })
-
-    window.addEventListener('click', (event) => {
-      if(this.objectsIntersected.length > 0) {
-        console.log(this.objectsIntersected[0])
-      }
-    })
-  }
-
 
   setupRenderer() {
     this.renderer = new THREE.WebGLRenderer({canvas: this.$refs.webgl, alpha: true});
@@ -170,9 +208,6 @@ export default class ClassicScrollBasedAnimation extends Vue {
     // Debug
     this.debug()
 
-    // Mouse tracker
-    this.setupMouse()
-
     //camera
     this.camera.position.z = 3
     this.scene.add(this.camera)
@@ -192,6 +227,11 @@ export default class ClassicScrollBasedAnimation extends Vue {
 
     // Full screen
     fullscreenUtil(this.$refs.webgl)
+
+    // Scroll
+    window.addEventListener('scroll', () => {
+      this.scrollY = window.scrollY
+    })
 
     const root = document.documentElement
     const rootBody = document.body
