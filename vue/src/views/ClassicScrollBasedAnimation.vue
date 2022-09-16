793245a0928f0e50ae2898557ef431c5f75766d1
mBrossois <git@github.com:mBrossois>
2022-09-16 09:34:38 +0200

    Added scroll-based animation

--- vue/src/views/ClassicScrollBasedAnimation.vue
+++ vue/src/views/ClassicScrollBasedAnimation.vue
@@ -15,11 +15,12 @@
 <script lang="ts">
 import {Options, Vue} from "vue-class-component";
 import * as THREE from "three";
-import {BoxGeometry, ConeGeometry, DirectionalLight, Material, Mesh, MeshBasicMaterial, MeshLambertMaterial, MeshStandardMaterial, MeshToonMaterial, NearestFilter, PerspectiveCamera, Points, Scene, Texture, TorusGeometry, TorusKnotGeometry, Vector2, WebGLRenderer} from "three";
+import {BoxGeometry, BufferAttribute, BufferGeometry, ConeGeometry, DirectionalLight, Group, Material, Mesh, MeshBasicMaterial, MeshLambertMaterial, MeshStandardMaterial, MeshToonMaterial, NearestFilter, PerspectiveCamera, Points, PointsMaterial, Scene, Texture, TorusGeometry, TorusKnotGeometry, Vector2, WebGLRenderer} from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import {FullScreenDocument, FullScreenDocumentElement} from "@/types/fullscreen.type";
 import {fullscreenUtil} from "@/utils/fullscreen.util";
 import * as dat from 'lil-gui'
+import gsap from 'gsap';
 import {RectAreaLightHelper} from "three/examples/jsm/helpers/RectAreaLightHelper";
 import { tsThisType } from "@babel/types";
 import { randFloat } from "three/src/math/MathUtils";
@@ -36,6 +37,7 @@ export default class ClassicScrollBasedAnimation extends Vue {
   renderer: WebGLRenderer = {} as WebGLRenderer;
   // fov between 45 & 75 || only shows objects between near and far values
   camera: PerspectiveCamera = new THREE.PerspectiveCamera(75, this.sizes.width / this.sizes.height, 0.1, 100);
+  cameraGroup: Group
 
   // Lights
   directionalLight: DirectionalLight
@@ -59,33 +61,53 @@ export default class ClassicScrollBasedAnimation extends Vue {
 
   // Parameters
   parameters = {
-    materialColor: '#ffeded'
+    materialColor: '#f0e13d'
   }
 
   // Mouse
   mouse:Vector2
+  cursor: {x: number, y: number}
+
+  // Particles
+  particleGeometry: BufferGeometry
+  particleMaterial: PointsMaterial
+  particles: Points
+
+  particleCount: number
 
   // Textures
   gradientTexture: Texture
-
+  particleTexture: Texture
+  
   gui = new dat.GUI({width: 300})
 
   doc = document as FullScreenDocument
 
-  clock = new THREE.Clock();
+  clock = new THREE.Clock()
+
+  previousTime = 0
 
   // Scroll
   scrollY = window.scrollY
+  currentSection = 0
 
   tick() {
     const elapsedTime = this.clock.getElapsedTime()
+    const deltaTime = elapsedTime - this.previousTime
+    this.previousTime = elapsedTime
 
     // Move camera
     this.camera.position.y = - this.scrollY / this.sizes.height * this.objectsDistance
 
+    const parallexX = this.cursor.x * 0.5
+    const parallexY = - this.cursor.y * 0.5
+
+    this.cameraGroup.position.x += (parallexX - this.cameraGroup.position.x) * 5 * deltaTime
+    this.cameraGroup.position.y += (parallexY - this.cameraGroup.position.y) * 5 * deltaTime
+
     for(const meshObject of this.sectionMeshes) {
-      meshObject.rotation.x = elapsedTime * 0.1
-      meshObject.rotation.y = elapsedTime * 0.12
+      meshObject.rotation.x += deltaTime * 0.1
+      meshObject.rotation.y += deltaTime * 0.12
     }
 
     //renderer
@@ -99,6 +121,7 @@ export default class ClassicScrollBasedAnimation extends Vue {
 
     this.gui.addColor(this.parameters, 'materialColor').onChange(() => {
       this.material.color.set(this.parameters.materialColor)
+      this.particleMaterial.color.set(this.parameters.materialColor)
     })
 
   }
@@ -155,6 +178,33 @@ export default class ClassicScrollBasedAnimation extends Vue {
 
   setupParticles() {
     console.log('particles')
+    this.particleCount = 200
+
+    this.particleGeometry = new BufferGeometry()
+
+    const positions = new Float32Array(this.particleCount * 3)
+
+    for(let i = 0; i < this.particleCount; i++) {
+      positions[i * 3] = (Math.random() - 0.5) * 10
+      positions[i * 3 + 1] = this.objectsDistance * 0.5 - Math.random() * this.objectsDistance * this.sectionMeshes.length
+      positions[i * 3 + 2] = (Math.random() - 0.5) * 10
+    }
+
+    this.particleGeometry.setAttribute('position', new BufferAttribute(positions, 3))
+
+    this.particleMaterial = new PointsMaterial({
+      alphaMap: this.particleTexture,
+      transparent: true, 
+      depthWrite: false,
+      blending: THREE.AdditiveBlending,
+      color: this.parameters.materialColor,
+      sizeAttenuation: true,
+      size: 0.03,
+    })
+
+    this.particles = new Points(this.particleGeometry, this.particleMaterial)
+
+    this.scene.add(this.particles)
 
   }
 
@@ -165,6 +215,8 @@ export default class ClassicScrollBasedAnimation extends Vue {
     // gradient
     this.gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
     this.gradientTexture.magFilter = NearestFilter
+
+    this.particleTexture = textureLoader.load('/textures/particles/3.png')
 
   }
 
@@ -185,6 +237,15 @@ export default class ClassicScrollBasedAnimation extends Vue {
       this.renderer.setSize(this.sizes.width, this.sizes.height)
 
       this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    })
+  }
+
+  setupMouse() {
+    this.cursor = {x: 0, y: 0}
+
+    window.addEventListener('mousemove', (e) => {
+      this.cursor.x = e.clientX / this.sizes.width - 0.5
+      this.cursor.y = e.clientY / this.sizes.height - 0.5
     })
   }
 
@@ -209,8 +270,11 @@ export default class ClassicScrollBasedAnimation extends Vue {
     this.debug()
 
     //camera
+    this.cameraGroup = new Group()
+    this.scene.add(this.cameraGroup)
+
     this.camera.position.z = 3
-    this.scene.add(this.camera)
+    this.cameraGroup.add(this.camera)
 
     // Axes helper
     // const axesHelper = new THREE.AxesHelper()
@@ -222,6 +286,9 @@ export default class ClassicScrollBasedAnimation extends Vue {
     // Resizing
     this.resizePage()
 
+    // Mouse
+    this.setupMouse()
+
     // Tick
     this.tick()
 
@@ -231,6 +298,41 @@ export default class ClassicScrollBasedAnimation extends Vue {
     // Scroll
     window.addEventListener('scroll', () => {
       this.scrollY = window.scrollY
+
+      const newSection = Math.round(this.scrollY / this.sizes.height)
+
+
+      if(newSection != this.currentSection) {
+        this.currentSection = newSection
+
+
+        gsap.to(
+          this.sectionMeshes[this.currentSection].rotation,
+          {
+            duration: 1.5,
+            ease: 'power2.inOut',
+            x: '+=6',
+            y: '+=3',
+            z: '+=1.5'
+          }
+        )
+
+        gsap.to(
+          this.sectionMeshes[this.currentSection].position,
+        {
+          y: - this.objectsDistance * (this.currentSection + 1),
+          duration: 0.5,
+          delay: 0.4
+        })
+
+        gsap.to(
+          this.sectionMeshes[this.currentSection].position,
+        {
+          y: - this.objectsDistance * this.currentSection,
+          duration: 0.5,
+          delay: 1
+        })
+      }
     })
 
     const root = document.documentElement
